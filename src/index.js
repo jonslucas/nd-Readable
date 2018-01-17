b1e4aaf40e6e42fba52599884e16ab4dbb759351
Jon Lucas <jon.s.lucas@gmail.com>
2018-01-17 18:14:00 -0500

    feat: Wire in redux-thunk middleware.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './components/App';
 import reducer from './reducers';
@@ -11,7 +12,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
 );
 
 ReactDOM.render(
