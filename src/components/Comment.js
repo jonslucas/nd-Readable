15d7fc2074af97f2a5c161710b8d37a819c80ba7
Jon Lucas <jon.s.lucas@gmail.com>
2018-01-16 21:29:03 -0500

    feat: Added edit button to comments. Added timestamp parsing.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { VoteComm } from './Vote';
 
 export const Comment = (props) => {
-  const { comment } = props;
-  console.log(JSON.stringify(comment, null, 2));
+  const { comment, edit } = props;
+  // console.log(JSON.stringify(comment, null, 2));
+  const date = new Date(comment.timestamp);
   return (
     <div className="comment-container">
       <VoteComm id={comment.id} />
       <div><p>{comment.body}</p></div>
       <div><p>{comment.author}</p></div>
-      <div><p>{comment.timestamp}</p></div>
+      <div><p>{date.toDateString()}</p></div>
+      <a href="#comment-edit" onClick={(e)=>edit(comment.id)}> Edit </a>
     </div>
   );
 }
